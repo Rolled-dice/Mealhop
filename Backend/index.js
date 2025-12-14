c5c757d9d1dc0c7443c6f43e3c5f971e67a65402
MOHIT_DUTT <mdutt0833@gmail.com>
2025-12-15 00:57:13 +0530

    Add initial frontend project files

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json()); // for the body pasrer.
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
